166c8c30e6a8ed175cef1997d72ac820f129ba86
guitardev <guitar.devmode@gmail.com>
2022-10-06 16:40:01 +0700

    edit model

--- datagov/admin.py
+++ datagov/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
+from .models import PersonGov60,PersonHouse
 
-from .models import gov_nid
-admin.site.register(gov_nid)
+
+@admin.register(PersonHouse)
+class PersonHouseAdmin(admin.ModelAdmin):
+    list_display = ['house_code','house_addr']
+
+@admin.register(PersonGov60)
+class PersonGov60Admin(admin.ModelAdmin):
+    list_display = ['nnid','house_code','title','f_name','l_name','sex','birth_date','body_code']
+        
--- datagov/models.py
+++ datagov/models.py
@@ -1,46 +1,42 @@
 from django.db import models
 from django.utils.translation import gettext as _
-class  gov_nid (models.Model):
-    nnid = models.CharField(_("nnid"),max_length = 20,null=False)
-    title = models.CharField(_("title"), max_length=50,null=False)
-    f_name = models.CharField(_("f_name"), max_length=100,null=False)
-    l_name = models.CharField(_("l_name"),max_length=100, null=False)
-    birth_date = models.CharField(_("birth_date"),max_length=100, null=True)
-    sex = models.CharField(_("sex"), max_length=50,null=True)
-    body_code = models.CharField(_("body_code"), max_length=2,null=True)	
-    house_code = models.CharField(_("house_code"), max_length=20,null=True)
-    class Meta:
-        ordering = ['-house_code']
-        db_table = 'gov_nid'
-    
-    def __str__(self):
-        return self.nnid,self.f_name,self.l_name
 
+class PersonHouse(models.Model):
+    house_code = models.IntegerField(_("House code"))
+    house_addr = models.CharField(_("House addr"),max_length=50)
 
-
-
-    
 
     class Meta:
-        verbose_name = _("gov_addr")
-        verbose_name_plural = _("gov_addrs")
+        verbose_name = _("PersonHouse")
+        verbose_name_plural = _("PersonHouses")
 
     def __str__(self):
-        return self.name
+        return str(self.house_code)
 
     def get_absolute_url(self):
-        return reverse("gov_addr_detail", kwargs={"pk": self.pk})
-
-
+        return reverse("PersonHouse_detail", kwargs={"pk": self.pk})
+# ฐานข้อมูลบุคคลที่มีบัตรสวัสดิการแห่งรัฐ 2560
+class PersonGov60(models.Model):
+    
+    nnid = models.CharField(_("nnid"), max_length=50)
+    title = models.CharField(_("Title"), max_length=100)
+    f_name = models.CharField(_("Firstname"), max_length=200)
+    l_name = models.CharField(_("Lastname"), max_length=200)
+    birth_date = models.DateField(_("Birthday"),blank=True, null=True)
+    sex = models.CharField(_("Sex"), max_length=50,null=True)
+    body_code = models.CharField(_("Body_code"), max_length=2,null=True)	
+    house_code = models.ForeignKey(PersonHouse, on_delete=models.CASCADE)
     
 
     class Meta:
-        verbose_name = _("")
-        verbose_name_plural = _("s")
+        verbose_name = _("PersonGov60")
+        verbose_name_plural = _("PersonGov60s")
 
     def __str__(self):
-        return self.name
+        return str(self.title)+str(self.f_name)+" "+str(self.l_name)
 
     def get_absolute_url(self):
-        return reverse("_detail", kwargs={"pk": self.pk})
+        return reverse("PersonGov60_detail", kwargs={"pk": self.pk})
+
+   
 
